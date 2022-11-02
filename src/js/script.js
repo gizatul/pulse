08daa33f2c7d74d549e25680ed973622c8927441
gizatul <gizat-aidar@yandex.ru>
2022-11-02 22:22:41 +0500

    tiny-slider without adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,17 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/arrows/prev_arrow.png"></button>',
-        nextArrow:'<button type="button" class="slick-next"><img src="../img/arrows/next_arrow.png"></button>',
-        responsive: [
-            {
-            breakpoint: 992,
-            settings: {
-            arrows: false,
-            dots: true
-            }
-        }
-        ]
-      });
+const slider = tns({
+    container: '.carousel_inner',
+    controls: false,
+    nav: false,
+    controlsText: [
+        '<img src="../img/arrows/prev_arrow.png">',
+        '<img src="../img/arrows/next_arrow.png">'
+    ]
   });
+
+  document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  });
+  
