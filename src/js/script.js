e6412e14bc860735c6bb2e91b6b1dc4bf9bccc6c
gizatul <gizat-aidar@yandex.ru>
2022-11-08 12:02:35 +0500

    +mailer

--- src/js/script.js
+++ src/js/script.js
@@ -86,6 +86,27 @@ const slider = tns({
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
+    //Mailer
+    $('form').submit(function(e) {
+      e.preventDefault();
+
+      if (!$(this).valid()) {
+        return;
+      }
+
+      $.ajax ({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+        $('form').trigger ('reset');
+      });
+      return false;
+    });
+
   })(jQuery);
 
       
